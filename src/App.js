bf49e32b50ab3fdf42f77193716f80585a99914d
Shubham <shubhamkhale301@gmail.com>
2023-08-12 15:30:21 +0530

    added existing project

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import Body from "./components/Body";
+import { Provider } from "react-redux";
+import store from "./store";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <Header />
+        <div className="flex">
+          <Sidebar />
+          <Body />
+        </div>
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className="App-header p-3 bg-blue-100">
+      Header Component
+    </header>
+  )
+}
+
+export default Header
